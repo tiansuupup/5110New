06fb1734f091ba0a8968b4345e81896531d80451
Mao-Hao <mao_hao@outlook.com>
2021-11-29 02:37:30 -0500

    door

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+
+    Animator anim;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter( Collider other )
+    {
+        anim.SetBool( "opened", true );
+
+    }
+
+    void OnTriggerExit( Collider other )
+    {
+        anim.enabled = true;
+        anim.SetBool( "opened", false );
+    }
+
+    void PauseAnimation()
+    {
+        anim.enabled = false;
+    }
+}
